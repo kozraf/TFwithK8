c5d74ffef7bc02928628846e168fd189d95093f9
kozraf <52557563+kozraf@users.noreply.github.com>
2023-03-01 22:32:25 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,3 +6,22 @@ sudo install -o root -g root -m 644 hashicorp.gpg /etc/apt/trusted.gpg.d/
 sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main" -y
 
 sudo apt install terraform
+
+sudo apt install docker-ce
+
+sudo tee /home/vagrant/dockerfile <<EOF
+FROM ubuntu:18.04
+ENV DEBIAN_FRONTEND=noninteractive
+RUN apt-get update && \
+  apt-get install -y gcc python-dev libkrb5-dev && \
+  apt-get install python3-pip -y && \
+  pip3 install --upgrade pip && \
+  pip3 install --upgrade virtualenv && \
+  pip3 install pywinrm[kerberos] && \
+  apt install krb5-user -y && \ 
+  pip3 install pywinrm && \
+  pip3 install ansible
+EOF
+
+sudo docker build -t ansible:latest .
+
